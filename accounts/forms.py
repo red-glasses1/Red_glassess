35c0c86912505c8e7b2237369fb5305f23cca6f4
JeonggonCho <wjdrhs62@gmail.com>
2023-05-15 17:54:58 +0900

    Edit login,signup

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,16 @@
-from accounts.models import Profile
+# from accounts.models import Profile
+from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from django.contrib.auth import get_user_model
 
 
-class ProfileForm(forms.ModelForm):
-  class Meta:
-    model = Profile
-    fields = '__all__'
+# class ProfileForm(forms.ModelForm):
+#   class Meta:
+#     model = Profile
+#     fields = '__all__'
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = get_user_model()
+        fields = ('username', 'email', 'password1', 'password2',)
