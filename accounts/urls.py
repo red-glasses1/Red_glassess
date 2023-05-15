77cfd51225f7d8f02549c68aae09b8fb158de11c
JeonggonCho <wjdrhs62@gmail.com>
2023-05-15 18:15:02 +0900

    Recommit for migration

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from accounts import views
 app_name = 'accounts'
 urlpatterns = [
   path('signup/', views.signup, name='signup'),
-  path('profile/edit/', views.profile_edit, name='profile_edit'),
+  # path('profile/edit/', views.profile_edit, name='profile_edit'),
   path('profile/', views.profile, name='profile'),
   path('login/', views.login, name='login'),
   path('logout/', views.logout, name='logout'),
