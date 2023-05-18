5b16535e063917a612bc5a703bf9784c0229f427
JeonggonCho <wjdrhs62@gmail.com>
2023-05-18 17:23:55 +0900

    Add profile/features

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,19 +1,31 @@
 # from accounts.models import Profile
-from django.contrib.auth.forms import UserCreationForm
-from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
+from django import forms
 
-from accounts.models import Profile
 
-
-class ProfileForm(forms.ModelForm):
-  class Meta:
-    model = Profile
-    fields = ['nickname','photo','follower_set']
-    widgets = {
-      'follower_set' : forms.Select,
-    }
-
+class ProfileForm(UserChangeForm):
+  username = forms.CharField(
+        label='아이디',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control w-100 p-2 mb-2',
+                'placeholder': '아이디',
+            },
+        ),
+    )
+    
+  image = forms.ImageField(
+        label='이미지',
+        widget=forms.ClearableFileInput(
+            attrs={
+                'class': 'form-control',
+            },
+        )
+    )
+  class Meta(UserChangeForm.Meta):
+    model = get_user_model()
+    fields = ('username', 'photo')
 
 
 class CustomUserCreationForm(UserCreationForm):
