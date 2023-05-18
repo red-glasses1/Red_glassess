ccc8ad2924ebfa17f6146b1f92582fce98ad2928
JeonggonCho <wjdrhs62@gmail.com>
2023-05-19 04:31:08 +0900

    Change DEBUG

--- red_glasses/settings.py
+++ red_glasses/settings.py
@@ -33,9 +33,9 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['port-0-red-glassess-13aenn2blhthwfuc.sel4.cloudtype.app']
+ALLOWED_HOSTS = ['port-0-red-glassess-13aenn2blhthwfuc.sel4.cloudtype.app', '127.0.0.1']
 
 # Application definition
 
