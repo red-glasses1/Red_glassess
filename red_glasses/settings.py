e3fa139df3ad13edb3e27c45481069e3104b9985
mangji12 <dhckddls12@naver.com>
2024-01-09 18:54:31 +0900

    호스트 업데이트

--- red_glasses/settings.py
+++ red_glasses/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+
 # from django.contrib.staticfiles.management.commands.runserver import Command as StaticFilesRunserverCommand
 # from sslserver.management.commands.runsslserver import Command as SSLServerCommand
 from django.urls import reverse_lazy
-
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,101 +28,104 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['port-0-red-glassess-3wh3o2blr663egz.sel5.cloudtype.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = [
+    "https://port-0-red-glassess-3wh3o2blr663egz.sel5.cloudtype.app/",
+    "127.0.0.1",
+    "localhost",
+]
 
 # Application definition
 
 INSTALLED_APPS = [
-    'posts',
-    'accounts',
-    'django_extensions',
-    'bootstrap4',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.sites',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'admin_honeypot',
-
-    #allauth(소셜로그인)
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-
-    #provider(소셜로그인 api 제공자)
-    'allauth.socialaccount.providers.kakao',
-    'allauth.socialaccount.providers.google',
-    'allauth.socialaccount.providers.naver',
+    "posts",
+    "accounts",
+    "django_extensions",
+    "bootstrap4",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.sites",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "admin_honeypot",
+    # allauth(소셜로그인)
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    # provider(소셜로그인 api 제공자)
+    "allauth.socialaccount.providers.kakao",
+    "allauth.socialaccount.providers.google",
+    "allauth.socialaccount.providers.naver",
 ]
 
 SOCIALACCOUNT_PROVIDERS = {
-    'naver': {
-        'APP': {
-            'client_id': os.getenv('NAVER_ID'),
-            'secret': os.getenv('NAVER_SECRET'),
+    "naver": {
+        "APP": {
+            "client_id": os.getenv("NAVER_ID"),
+            "secret": os.getenv("NAVER_SECRET"),
         }
     },
-    'google': {
+    "google": {
         "APP": {
-            "client_id": os.getenv('GOOGLE_ID'),
-            "secret": os.getenv('GOOGLE_SECRET'),
+            "client_id": os.getenv("GOOGLE_ID"),
+            "secret": os.getenv("GOOGLE_SECRET"),
         }
     },
-    'kakao': {
+    "kakao": {
         "APP": {
-            "client_id": os.getenv('KAKAO_ID'), #REST API 키
-            "secret": os.getenv('KAKAO_ID'), #내 애플리케이션 -> 보안
+            "client_id": os.getenv("KAKAO_ID"),  # REST API 키
+            "secret": os.getenv("KAKAO_ID"),  # 내 애플리케이션 -> 보안
         }
-    }
+    },
 }
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'red_glasses.urls'
+ROOT_URLCONF = "red_glasses.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'red_glasses.wsgi.application'
+WSGI_APPLICATION = "red_glasses.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -132,16 +135,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -149,9 +152,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'ko-Kr'
+LANGUAGE_CODE = "ko-Kr"
 
-TIME_ZONE = 'Asia/Seoul'
+TIME_ZONE = "Asia/Seoul"
 
 USE_I18N = True
 
@@ -161,35 +164,35 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR)
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
 # ]
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / "static",
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-AUTH_USER_MODEL = 'accounts.Profile'
+AUTH_USER_MODEL = "accounts.Profile"
 
 AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend',
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
 ]
 
 SITE_ID = 1
 SOCIALACCOUNT_LOGIN_ON_GET = True
 ACCOUNT_LOGOUT_ON_GET = True
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
 LOGIN_URL = "/"
 # class Command(StaticFilesRunserverCommand, SSLServerCommand):
