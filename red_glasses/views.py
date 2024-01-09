a0fe272f41aa3aabaa111c2c6d202df024e6e12f
mangji12 <dhckddls12@naver.com>
2024-01-09 19:50:44 +0900

    호스트 업데이트

--- red_glasses/views.py
+++ red_glasses/views.py
@@ -2,42 +2,45 @@ from django.shortcuts import render
 import requests
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
+
 
 def index(request):
     popular_url = "https://api.themoviedb.org/3/movie/popular"
 
     params = {
-        "api_key": os.getenv('TMDB_API'),
+        "api_key": "ad54a63bd524b655ee79f3f3108ba520",  # os.getenv('TMDB_API'),
         "language": "ko",
         "page": "1",
         "region": "KR",
-        }
-    
+    }
+
     popular_response = requests.get(popular_url, params=params)
     popular_info = popular_response.json()
-    popular_movies = sorted(popular_info['results'], key=lambda x: -x['vote_average'])
+    popular_movies = sorted(popular_info["results"], key=lambda x: -x["vote_average"])
     for i, popular_movie in enumerate(popular_movies, start=1):
-        popular_movie['rank'] = i
-
+        popular_movie["rank"] = i
 
     now_playing_url = "https://api.themoviedb.org/3/movie/now_playing"
 
     now_playing_response = requests.get(now_playing_url, params=params)
     now_playing_info = now_playing_response.json()
-    now_playing_movies = sorted(now_playing_info['results'], key=lambda x: -x['vote_average'])
+    now_playing_movies = sorted(
+        now_playing_info["results"], key=lambda x: -x["vote_average"]
+    )
     for i, now_playing_movie in enumerate(now_playing_movies, start=1):
-        now_playing_movie['rank'] = i
+        now_playing_movie["rank"] = i
 
     upcoming_url = "https://api.themoviedb.org/3/movie/upcoming"
 
     upcoming_response = requests.get(upcoming_url, params=params)
     upcoming_info = upcoming_response.json()
-    upcoming_movies = sorted(upcoming_info['results'], key=lambda x: x['release_date'])
+    upcoming_movies = sorted(upcoming_info["results"], key=lambda x: x["release_date"])
 
     context = {
-        'popular_movies': popular_movies,
-        'now_playing_movies': now_playing_movies,
-        'upcoming_movies': upcoming_movies,
+        "popular_movies": popular_movies,
+        "now_playing_movies": now_playing_movies,
+        "upcoming_movies": upcoming_movies,
     }
-    return render(request, 'index.html', context)
+    return render(request, "index.html", context)
